bfdc456ba405417e17696f2d7b136a24748cfbb9
jgroen <josh.groen@gmail.com>
2016-11-29 18:14:28 -0600

    Update run.sh

--- chute/run.sh
+++ chute/run.sh
@@ -5,16 +5,10 @@ mkdir -p /var/www/html/CPs
 chmod a+rw /var/www/html/CPs
 
 # Execute the file, one pic every 10 ms
-python /usr/local/bin/ar_nav_2.py -m_sec 10 > seccam.log 2> seccam.err &
-
-# Add the symlink
-ln -s --relative /var/www/html/CPs /var/www/html/app-dist/
+python /usr/local/bin/TestServer_v2.py &
 
 # Allow client traffic for development
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-
-# Enable mod rewrite
-/usr/sbin/a2enmod rewrite
 
 while true; do
     sleep 300
