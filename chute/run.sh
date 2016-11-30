dad412253d86783c1061eb0679a40ed48d488e02
jgroen <josh.groen@gmail.com>
2016-11-29 22:58:08 -0600

    Update run.sh

--- chute/run.sh
+++ chute/run.sh
@@ -6,9 +6,6 @@ python /usr/local/bin/TestServer_v2.py &
 # Allow client traffic for development
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
-# Enable mod rewrite
-/usr/sbin/a2enmod rewrite
-
 while true; do
     sleep 300
 done
